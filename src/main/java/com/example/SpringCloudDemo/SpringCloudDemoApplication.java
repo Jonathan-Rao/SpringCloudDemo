512d0522a38d7a326c33a7e67cede87eea6273fb
raodaokun <l>
2018-08-30 13:51:10 +0800

    ...

--- src/main/java/com/example/SpringCloudDemo/SpringCloudDemoApplication.java
+++ src/main/java/com/example/SpringCloudDemo/SpringCloudDemoApplication.java
@@ -9,6 +9,6 @@ public class SpringCloudDemoApplication {
 	public static void main(String[] args) {
 		System.out.println("进来了");
 		SpringApplication.run(SpringCloudDemoApplication.class, args);
-		System.out.println("进来了");
+		System.out.println("进来了");  
 	}
 }
